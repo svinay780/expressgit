b7393fdb45e554cb5d34e042b2ef01ab95092c20
Vinay Singh <svinay780@gmail.com>
2024-07-30 17:55:46 +0530

    adding

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const app=express()
+const port=8080
+const path=require("path")
+app.use(express.static(path.join(__dirname,"public")))
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+app.listen(port,()=>{
+    console.log("check is it Working")
+}
+)
+app.get("/home",(req,res)=>{
+    let {user}=req.query
+    res.render("home.ejs",{user})
+})
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
